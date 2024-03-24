ed5000b80ab48bebeba4d6a77bd2f6d7f27486a3
bullinnyc <i8u11@icloud.com>
2024-03-24 19:58:47 +0700

    Initial Commit

---
+++ OTP/OTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTPApp.swift
+//  OTP
+//
+//  Created by Dmitry Kononchuk on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct OTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
